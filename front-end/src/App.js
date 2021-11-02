ae8ba3d9d7601d8c1d2cc610d10e453deda6d263
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-11-02 14:18:53 -0400

    css: ajust

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -46,19 +46,19 @@ function App() {
       <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
         <img src="https://i.ibb.co/KjrPCyW/map.png" className="w-full xl:h-full h-96 object-cover object-fill sm:block hidden" alt=""/>
         <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" className="sm:hidden -mt-10 block w-full h-96 object-cover object-fill absolute z-0" alt="" />
-        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
+        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12 rounded">
             <p className="text-3xl font-semibold text-gray-800">{ weather['main']['temp'] }°</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-yellow-600">Temperatura atual</p>
         </div>
-        <div className="shadow-lg xl:p-6 p-4 w-48 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
+        <div className="shadow-lg xl:p-6 p-4 w-48 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12 rounded">
             <p className="text-3xl font-semibold text-gray-800">{ weather['main']['temp_max'] }°</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-yellow-600">Temperatura máxima</p>
         </div>
-        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24">
+        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24 rounded">
             <p className="text-3xl font-semibold text-gray-800">{ weather['main']['temp_min'] }°</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-yellow-600">Temperatura minima</p>
         </div>
-        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-black sm:absolute relative z-20 md:mt-0 sm:-mt-0 mt-4 top-0 xl:mr-56 sm:mr-24">
+        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full dark:bg-gray-900 sm:absolute relative z-20 md:mt-0 sm:-mt-0 mt-4 top-0 xl:mr-56 sm:mr-24 rounded">
             <p className="text-3xl font-semibold text-white">{ weather['main']['humidity'] }%</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-yellow-600">Umidade da localidade</p>
         </div>
